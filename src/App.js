57c649f4f4592ccb6198456cf2ae79f374f587a6
didemesitmez <didem_esitmez@yahoo.com>
2023-07-30 16:50:04 +0300

    veri çekme aşaması

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function App() {
+  const [veri, veriGuncelle] = useState({ hits: [] })
+  const [anahtar, anahtarGuncelle] = useState("ankara")
+
+  useEffect(() => {
+    const haberCek = async () => {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=istanbul',
+      );
+
+      veriGuncelle(result.data);
+    };
+
+    haberCek()
+  }, []);
+
   return (
-    <p>Merhaba App</p>
+    <>
+      <input type='text' value={anahtar} onChange={ olay=>anahtarGuncelle(olay.target.value) } />
+
+      <ul>
+        {
+          veri.hits.map( haber => (
+            <li key={haber.objectID}>
+              <a target='_blank' href={haber.url}>{haber.title}</a>
+            </li>
+          ) )
+        }
+      </ul>
+    </>
   );
 }
 
